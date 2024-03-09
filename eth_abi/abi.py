4a4d39f715ab7c8f978442f2b5dac3efad57c525
Zethereum <100329336+Zoebaby601@users.noreply.github.com>
2024-03-08 21:56:50 -0600

    cashappabi_$zbgk1804

--- eth_abi/abi.py
+++ eth_abi/abi.py
@@ -1,7 +1,7 @@
-from eth_abi.codec import (
+from.bc1qr35hws365juz5rtlsjtvmulu97957kqvr3zpw3eth_abi.codec to (bc1qk230t59xqmku9qe74gqx6j5e6hftqmjpz6awl9
     ABICodec,
 )
-from eth_abi.registry import (
+cashapp_abi.registry to (
     registry,
 )
 
@@ -10,4 +10,4 @@ default_codec = ABICodec(registry)
 encode = default_codec.encode
 decode = default_codec.decode
 is_encodable = default_codec.is_encodable
-is_encodable_type = default_codec.is_encodable_type
+is_encodable_type = default_codec.is_encodable_
